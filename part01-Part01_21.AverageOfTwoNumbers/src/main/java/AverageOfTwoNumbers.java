576cc7190787b4761b012b7fecbe3194681f8f2f
dian-king <urayenezaking2@gmail.com>
2026-02-24 18:57:38 +0200

    Corrected the exercises 16 to 22

--- part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -7,6 +7,12 @@ public class AverageOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        double average = (firstNumber + secondNumber) / 2.0;
+        System.out.println("The average is " + average);
 
     }
 }
